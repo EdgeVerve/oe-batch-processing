86e0c630008354074dbcdca0e9f4b2a41c109846
ajith <ajith_v@edgeverve.com>
2018-05-18 11:15:37 +0530

    Added documentation/comments

--- test/batch-process-tests.js
+++ test/batch-process-tests.js
@@ -23,8 +23,8 @@ describe("batch-processing-tests", function () {
     it('should call processFile', function (done) {
         log.debug("calling processFile");
 
-        var filePath = "test/testdata.1.txt";
-//        var filePath = "D:/1k.txt";
+//        var filePath = "test/testdata.1.txt";
+        var filePath = "D:/1k.txt";
 //        var filePath = "D:/20k.txt";
 //        var filePath = "D:/100mil.txt";
         var options = { 
