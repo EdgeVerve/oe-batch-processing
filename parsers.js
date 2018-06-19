2f89aa5416b9d8860a707cd9f7dff875312692b2
ajith <ajith_v@edgeverve.com>
2018-06-19 09:08:00 +0530

    Added copyright notice

--- parsers.js
+++ parsers.js
@@ -1,12 +1,31 @@
+/*
+©2015-2016 EdgeVerve Systems Limited (a fully owned Infosys subsidiary), Bangalore, India. All Rights Reserved.
+The EdgeVerve proprietary software program ("Program"), is protected by copyrights laws, international treaties and other pending or existing intellectual property rights in India, the United States and other countries.
+The Program may contain/reference third party or open source components, the rights to which continue to remain with the applicable third party licensors or the open source community as the case may be and nothing here transfers the rights to the third party and open source components, except as expressly permitted.
+Any unauthorized reproduction, storage, transmission in any form or by any means (including without limitation to electronic, mechanical, printing, photocopying, recording or  otherwise), or any distribution of this Program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+*/
+/**
+ * This module provides 'built-in' functions for parsing data-files. File types currently supported are
+ * 1. Comma Separated Value (CSV) / delimited files
+ * 2. Fixed Width (FW) value files
+ * 
+ * For documentation on the usage of this module, please see README.md
+ * 
+ * @file parsers.js
+ * @author Ajith Vasudevan
+ */
+
+// Setup a environment variable for easy setup of logger configuration 
 if(!process.env["LOGGER_CONFIG"] && process.env["BATCH_LOGGER_CONFIG"]) {
     process.env["LOGGER_CONFIG"] = JSON.stringify({"levels":{"default":process.env["BATCH_LOGGER_CONFIG"].trim().toLocaleLowerCase()}});
 }
 var log = require('oe-logger')('parsers');
 var errmsg;
 
+// The CSV Parser function wrapper - exported from this file
 function csvParser(options) {
     var csvHeaders, csvHeaderDataTypes, allHeadersString = false;
-    this.onEachRecord = function onEachRecord (recData, cb) {
+    this.onEachRecord = function onEachRecord (recData, cb) {    // The parser function
         var payload, json = {};
         if(!csvHeaders) {
             if(options.csvHeaders) {  // if csvHeaders were supplied in some form
@@ -136,11 +155,11 @@ function CSVtoArray(text) {
     return a;
 };
 
-
+// The CSV Parser function wrapper - exported from this file
 function fwParser(options) {
     var fwOptHeaders, fwHeaders, fwHeaderDataTypes, allHeadersString = false;
 
-    this.onEachRecord = function onEachRecord (recData, cb) {
+    this.onEachRecord = function onEachRecord (recData, cb) {   // The parser function
         var payload, json = {};
         var errMsg, error, pos = 0;
         if(!fwHeaders) {
